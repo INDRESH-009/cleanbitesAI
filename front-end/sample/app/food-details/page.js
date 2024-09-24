67410b884791bd99d2acf8e42e9207861f114a3e
Indresh <indreshmr12345@gmail.com>
2024-09-24 10:22:49 +0530

    sampl

---
+++ front-end/sample/app/food-details/page.js
@@ -0,0 +1,18 @@
+
+import FoodOutput from '@/components/FoodDetails/FoodOutput'
+import FoodSearch from '@/components/FoodDetails/FoodSearch'
+import React from 'react'
+
+function FoodDetailsPage() {
+  return (
+    <div className=''>
+        <FoodSearch />
+        
+
+        
+        
+    </div>
+  )
+}
+
+export default FoodDetailsPage
---
+++ front-end/sample/app/user-details/page.js
@@ -0,0 +1,10 @@
+import UserDetails from '@/components/UserDetails/UserDetails'
+import React from 'react'
+
+function UserDetailsPage() {
+  return (
+    <UserDetails />
+  )
+}
+
+export default UserDetailsPage
